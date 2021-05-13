e197a7eb63bd60dfda96e890d9904b4c3be4a556
MancillaLeandro <84150144+MancillaLeandro@users.noreply.github.com>
2021-05-13 17:52:14 -0300

    Add files via upload

---
+++ TP 1/Actividad 3/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float x;
+    cout << "Conversor De Velocidad De km/h a m/s" << endl;
+    cout << "Ingrese La Velocidad En km/h" << endl;
+    cin >> x;
+    cout << "La Velocidad Es Igual A " << x/3.6 << " m/s" << endl;
+    return 0;
+}
